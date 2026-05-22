d15aaa27c540a9f0cd32deaf80b6e5edc6cf4222
Shubham Jaiswal <112782089+ShubhamJaiswal-Engg@users.noreply.github.com>
2026-05-23 00:09:42 +0530

    initialise sample data

--- init/app.js
+++ init/app.js
@@ -1,22 +1,30 @@
+require("dotenv").config({ path: require("path").resolve(__dirname, "../.env") });
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../model/listing.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/airbnb";
+const MONGO_URL = process.env.ATLASDB_URL;
 
 main().then(() => {
     console.log("connected DB");
+    // Initialize data only AFTER successful connection
+    initDB();
 }).catch((err) => {
     console.log(err);
-})
+});
+
 async function main() {
     await mongoose.connect(MONGO_URL);
 }
 
 const initDB = async () => {
     try {
-        await Listing.deleteMany({});
-        initData.data = initData.data.map((obj) => ({...obj, owner: "676e904ab74fe45f9986b6e7"}));
+        // await Listing.deleteMany({});
+        initData.data = initData.data.map((obj) => ({
+            ...obj, 
+            owner: "69f23fb4b0fe37deff48e623",
+            // geometry: { type: "Point", coordinates: [ 77.2090, 28.6139 ] } // Default dummy coordinates to prevent map crash
+        }));
         await Listing.insertMany(initData.data);
         console.log("Data was initialized");
     } catch (err) {
@@ -25,5 +33,3 @@ const initDB = async () => {
         mongoose.connection.close();
     }
 };
-
-initDB();
